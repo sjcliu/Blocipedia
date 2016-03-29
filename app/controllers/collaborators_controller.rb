3ea2e4c0a6d9433eca49fdf67061bc15eebc8cbf
Stanly Liu <sjcliu@gmail.com>
2016-03-29 20:06:40 +1100

    still working on collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -4,6 +4,10 @@ class CollaboratorsController < ApplicationController
   end
 
   def new
-    @collaborator = Collaborator.new
+    @collaborator = Collaborator.new.find_by(user_id: user.id, wiki_id: params[:wiki_id])
+  end
+
+  def index
+    @users = User.all
   end
 end
