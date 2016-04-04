d4e792bdd6d34fd0d3e77142689970d3705c21aa
Stanly Liu <sjcliu@gmail.com>
2016-04-04 18:47:03 +1000

    collaborator done

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -34,7 +34,7 @@ class WikiPolicy < ApplicationPolicy
        elsif user && user.role == 'premium'
          all_wikis = scope.all
          all_wikis.each do |wiki|
-           if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+           if wiki.public? || wiki.user == user || wiki.collaborators.include?(user)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
          end
@@ -42,7 +42,7 @@ class WikiPolicy < ApplicationPolicy
          all_wikis = scope.all
          wikis = []
          all_wikis.each do |wiki|
-           if wiki.public? || wiki.collaborators.include?(user)
+           if wiki.public? || wiki.collaborators.pluck(:user_id).include?(user.id)
              wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
            end
          end
